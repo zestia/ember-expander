3fc5616c0c535923e35416352c161c26a2820f37
Andrew Kirwin <andrew.kirwin@zestia.com>
2020-12-18 14:50:57 +0000

    upgrade deps

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 2.1.5
+
+- Upgrade dependencies
+
 ## 2.1.4
 
 - Run ember-cli-update
--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,10 +1,18 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
-export default Controller.extend({
-  show: true,
-  actions: {
-    toggle() {
-      this.toggleProperty('show');
-    },
-  },
-});
+export default class ApplicationController extends Controller {
+  @tracked show = true;
+  @tracked more = false;
+
+  @action
+  toggle() {
+    this.show = !this.show;
+  }
+
+  @action
+  toggleMore() {
+    this.more = !this.more;
+  }
+}
