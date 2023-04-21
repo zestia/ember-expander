c36b015f13366b9c311b39141e8bcabb67bd09f8
Andrew Kirwin <github@andrewkirwin.co.uk>
2023-04-21 09:27:09 +0100

    Use hash locationType in prod

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -43,6 +43,7 @@ module.exports = function (environment) {
   if (environment === 'production') {
     // here you can enable a production-specific feature
     ENV.rootURL = '/ember-expander/';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
